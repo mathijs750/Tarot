80a8eff9acefce411cc234905082d3f8af46ec66
Mathijs Lemmers <mathijs750@gmail.com>
2016-01-30 09:32:29 +0100

    character

--- Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private AnimationCurve SpeedCurve;
+    [SerializeField]
+    private float SpeedMultiplier;
+    private bool canRun = true;
+    private Vector3 movementDirection;
+    private CharacterController characterController;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        movementDirection = Vector3.zero;
+    }
+
+    void Update()
+    {
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementDirection = transform.TransformDirection(movementDirection);
+        movementDirection = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0) * movementDirection;
+        movementDirection = Vector3.ClampMagnitude(movementDirection, 1f);
+
+        if (Input.GetButton("fire1"))
+        {
+            movementDirection *= SpeedCurve.Evaluate(movementDirection.magnitude) * SpeedMultiplier;
+        }
+        else
+        {
+            movementDirection = Vector3.ClampMagnitude(movementDirection, .5f);
+            movementDirection *= SpeedCurve.Evaluate(movementDirection.magnitude) * SpeedMultiplier;
+        }
+
+        characterController.Move(movementDirection * Time.deltaTime);
+    }
 }
